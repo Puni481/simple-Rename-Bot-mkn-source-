a612b042047c7ed394fde1252604e901e2ce0bde
Puni481 <133667859+Puni481@users.noreply.github.com>
2023-05-18 19:00:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "24815725"))
+API_HASH = environ.get("API_HASH", "7887b946ba021b8fdd64176664cfc26b")
+BOT_TOKEN = environ.get("BOT_TOKEN", "5654736203:AAGRwvxYiOH8OzrWjss_KavDlVwicmVz3iI")
+ADMIN = int(environ.get("ADMIN", "6273043621"))          
+CAPTION = environ.get("CAPTION", "Join https://t.me/kannadacinemaji")
 
 class temp(object):
-    THUMBNAIL = environ.get("THUMBNAIL", "")
+    THUMBNAIL = environ.get("THUMBNAIL", "https://graph.org/file/fd6a0935f80237ac7e9e5.jpg")
